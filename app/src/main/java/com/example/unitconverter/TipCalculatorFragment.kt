3840173c564de1e488648c829447f174f7d35577
serayou <gyh02035@gmail.com>
2023-08-22 23:01:37 -0700

    팁 계산기 > 토탈 가격 입력 시 팁 퍼센트 계산 추가

--- app/src/main/java/com/example/unitconverter/TipCalculatorFragment.kt
+++ app/src/main/java/com/example/unitconverter/TipCalculatorFragment.kt
@@ -1,18 +1,49 @@
 package com.example.unitconverter
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.unitconverter.databinding.TipCalculatorFragmentBinding
 
 class TipCalculatorFragment : Fragment() {
+
+    private lateinit var binding: TipCalculatorFragmentBinding
+    private lateinit var mainActivity: MainActivity
+
+    private var totalDollar : Double? = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.tip_calculator_fragment, container, false)
+        binding = TipCalculatorFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setResource()
+    }
+
+    private fun setResource() {
+        binding.btnCheck.setOnClickListener{
+            totalDollar = binding.etTotal.text.toString().toDouble()
+            calculateTipPercent()
+        }
+    }
+
+    private fun calculateTipPercent(){
+        binding.tvTip15Dollar.text = String.format("%.2f", totalDollar?.times(0.15))
+        binding.tvTip18Dollar.text = String.format("%.2f", totalDollar?.times(0.18))
+        binding.tvTip20Dollar.text = String.format("%.2f", totalDollar?.times(0.2))
     }
 }
