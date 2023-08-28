bb8c994ed63964d2808fe8f9d067b6a45d8ccd76
serayou <gyh02035@gmail.com>
2023-08-27 23:43:13 -0700

    팁 계산기 Custom Tip 계산 기능 추가

--- app/src/main/java/com/example/unitconverter/TipCalculatorFragment.kt
+++ app/src/main/java/com/example/unitconverter/TipCalculatorFragment.kt
@@ -3,6 +3,9 @@ package com.example.unitconverter
 import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +21,7 @@ class TipCalculatorFragment : Fragment() {
     private lateinit var mainActivity: MainActivity
 
     private var totalDollar : Double? = 0.0
+    private var customInputType : String? = "dollarToPercent"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -69,6 +73,69 @@ class TipCalculatorFragment : Fragment() {
         binding.layoutBtnTip20.setOnClickListener {
             addTip(binding.tvTip20Dollar.text.toString())
         }
+
+        binding.btnTipCustom.setOnClickListener {
+            if(binding.etTotal.text.toString().isNullOrEmpty()){
+                Toast.makeText(mainActivity, getString(R.string.msg_input_total), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            initCustomTip()
+            setSelectTipLayoutVisibility(false)
+        }
+
+        binding.btnBackToSelectTip.setOnClickListener{
+            setSelectTipLayoutVisibility(true)
+            binding.tvTotalWithTip.text = null
+        }
+
+        binding.etTopCustomTip.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if(count == 0){
+                    binding.tvBottomCustomTip.text = null
+                    binding.tvTotalWithTip.text = null
+                }
+                if(binding.etTopCustomTip.text.isNotEmpty()){
+                    calculateCustomTip(customInputType, binding.etTopCustomTip.text.toString())
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        binding.btnSwitch.setOnClickListener {
+            binding.etTopCustomTip.text = null
+            binding.tvBottomCustomTip.text = null
+            if(binding.imageCustomTipTopDollar.visibility == View.VISIBLE){
+                customInputType = "PercentToDollar"
+                binding.imageCustomTipTopDollar.visibility = View.GONE
+                binding.imageCustomTipTopPercent.visibility = View.VISIBLE
+                binding.imageCustomTipBottomDollar.visibility = View.VISIBLE
+                binding.imageCustomTipBottomPercent.visibility = View.GONE
+            }else if(binding.imageCustomTipTopDollar.visibility == View.GONE){
+                customInputType = "dollarToPercent"
+                binding.imageCustomTipTopDollar.visibility = View.VISIBLE
+                binding.imageCustomTipTopPercent.visibility = View.GONE
+                binding.imageCustomTipBottomDollar.visibility = View.GONE
+                binding.imageCustomTipBottomPercent.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun initCustomTip(){
+        //Custom Tip
+        customInputType = "dollarToPercent"
+        binding.imageCustomTipTopDollar.visibility = View.VISIBLE
+        binding.imageCustomTipTopPercent.visibility = View.GONE
+        binding.imageCustomTipBottomDollar.visibility = View.GONE
+        binding.imageCustomTipBottomPercent.visibility = View.VISIBLE
+
+        binding.etTopCustomTip.text = null
+        binding.tvBottomCustomTip.text = null
+        binding.tvTotalWithTip.text = null
     }
 
     private fun calculateTipPercent(){
@@ -79,16 +146,56 @@ class TipCalculatorFragment : Fragment() {
         binding.tvTip20Dollar.text = String.format("%.2f", totalDollar?.times(0.2))
     }
 
+    private fun calculateCustomTip(calculateType : String?, input : String){
+        if(calculateType.equals("dollarToPercent")){
+            binding.tvBottomCustomTip.text = String.format("%.2f", (totalDollar?.let {
+                input.toDouble().div(
+                    it
+                )
+            })?.times(100.0))
+
+            addTip(binding.etTopCustomTip.text.toString())
+        }else if(calculateType.equals("PercentToDollar")){
+            binding.tvBottomCustomTip.text = String.format("%.2f", totalDollar?.times(input.toDouble().div(100.0)))
+
+            addTip(binding.tvBottomCustomTip.text.toString())
+        }
+    }
+
     private fun addTip(tip : String){
         tip.takeIf { it.isNotEmpty() }
             ?.toDoubleOrNull()
             ?.let { tipAmount ->
-                binding.tvTotalWithTip.text = totalDollar?.plus(tipAmount).toString()
-            } ?: Toast.makeText(mainActivity, "값을 입력해 주세요.", Toast.LENGTH_SHORT).show()
+                binding.tvTotalWithTip.text = String.format("%.2f", totalDollar?.plus(tipAmount))
+            } ?: Toast.makeText(mainActivity, getString(R.string.msg_input_total), Toast.LENGTH_SHORT).show()
     }
 
     private fun hideKeyboard(){
         val imm = mainActivity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(mainActivity.window.decorView.applicationWindowToken, 0)
+    }
+
+    private fun setSelectTipLayoutVisibility(visibility : Boolean){
+        if(visibility){
+            binding.layoutSelectTip.visibility = View.VISIBLE
+            binding.layoutSelectCustomTip.visibility = View.GONE
+        }else{
+            binding.layoutSelectTip.visibility = View.GONE
+            binding.layoutSelectCustomTip.visibility = View.VISIBLE
+        }
+    }
+
+    fun resetCalculator(){
+        setSelectTipLayoutVisibility(true)
+
+        binding.etTotal.text = null
+        binding.tvTotalWithTip.text = null
+
+        binding.tvTip15Dollar.text = null
+        binding.tvTip18Dollar.text = null
+        binding.tvTip20Dollar.text = null
+
+        totalDollar = 0.0
+        customInputType = "dollar"
     }
 }
